21a4c5cf980f3286ec8b865e5f8f4f2fc8175c03
redahabbat-cyber <redahabbat@gmail.com>
2025-10-29 09:14:11 +0100

    Delete MyCheatMenu directory

--- MyCheatMenu/globals.cpp
+++
@@ -1,6 +0,0 @@
-// globals.cpp
-#include "globals.h"
-
-HANDLE   g_hProcess = nullptr;
-DWORD    g_pid = 0;
-uintptr_t g_clientBase = 0;
--- MyCheatMenu/globals.h
+++
@@ -1,7 +0,0 @@
-// globals.h
-#pragma once
-#include <Windows.h>
-
-extern HANDLE   g_hProcess;
-extern DWORD    g_pid;
-extern uintptr_t g_clientBase;
